8b30cd92f0f80521ccd2768b15a1ad07c26c9a41
sanhaoqingnian <su2426694419@qq.com>
2019-05-30 10:37:10 +0800

    //新增实体类属性  消费类型

--- src/main/java/com/toughguy/binheSportSystem/model/content/Bill.java
+++ src/main/java/com/toughguy/binheSportSystem/model/content/Bill.java
@@ -6,21 +6,23 @@ package com.toughguy.binheSportSystem.model.content;
  *
  */
 public class Bill {
-	private int type;		//状态   1为充值   2为消费
-	private String cardId;		//卡号
-	private String money;		//消费金额
-	private String time;		//时间
+	private int type;				//状态   1为充值   2为消费
+	private String cardId;			//卡号
+	private String money;			//消费金额
+	private String time;			//时间
+	private String consumetype;		//消费类型
 	
 	public Bill() {
 		super();
 	}
 
-	public Bill(int type, String cardId, String money, String time) {
+	public Bill(int type, String cardId, String money, String time, String consumetype) {
 		super();
 		this.type = type;
 		this.cardId = cardId;
 		this.money = money;
 		this.time = time;
+		this.consumetype = consumetype;
 	}
 
 	public int getType() {
@@ -54,12 +56,20 @@ public class Bill {
 	public void setTime(String time) {
 		this.time = time;
 	}
+	
+	public String getConsumetype() {
+		return consumetype;
+	}
+
+	public void setConsumetype(String consumetype) {
+		this.consumetype = consumetype;
+	}
 
 	@Override
 	public String toString() {
-		return "Bill [type=" + type + ", cardId=" + cardId + ", money=" + money + ", time=" + time + "]";
+		return "Bill [type=" + type + ", cardId=" + cardId + ", money=" + money + ", time=" + time + ", consumetype="
+				+ consumetype + "]";
 	}
-	
 
 	
 	
