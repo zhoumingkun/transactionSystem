95e4231365c519253f4295c925e2da971405f5e9
sanhaoqingnian <su2426694419@qq.com>
2019-06-26 15:31:48 +0800

    //添加查询全部的场馆名称和场馆消费类型

--- src/main/java/com/toughguy/binheSportSystem/controller/content/MemberBillController.java
+++ src/main/java/com/toughguy/binheSportSystem/controller/content/MemberBillController.java
@@ -16,6 +16,7 @@ import com.toughguy.binheSportSystem.model.content.Bill;
 import com.toughguy.binheSportSystem.model.content.BillRequest;
 import com.toughguy.binheSportSystem.model.content.ConsumeBill;
 import com.toughguy.binheSportSystem.service.content.prototype.IMemberBillService;
+import com.toughguy.binheSportSystem.service.content.prototype.IReserveTimeService;
 
 /**
  * 查询用户账单controller类
@@ -29,6 +30,9 @@ public class MemberBillController {
 	@Autowired
 	private IMemberBillService memberBillservice;
 	
+	@Autowired
+	private IReserveTimeService reserveTimeService;
+	
 	
 	/**
 	 * 传入参数卡号和手机号 查询用户全部账单
@@ -37,9 +41,13 @@ public class MemberBillController {
 	 */
 	@RequestMapping("/findAllbill")
 	public List<Bill> findAll(@RequestParam(name = "cardnumber") String cardno, @RequestParam(name = "tel") String usermobile) {		//需要传递参数卡号 手机号 
+		List<String> type = reserveTimeService.findAllType();		//查询全部的场馆名称
+		List<String> Other = reserveTimeService.findAllOtherType();	//查询其他消费类型名称
 		Map<String,Object> map = new HashMap<String, Object>();		
 		map.put("cardno",cardno);
 		map.put("mobile",usermobile);
+		map.put("list", type);
+		map.put("other", Other);
 		List<Bill> list= new ArrayList<>();
 		List<ConsumeBill> recharge = memberBillservice.findAllRecharge(map);		//查询充值记录
 		for(ConsumeBill arr:recharge) {
@@ -62,7 +70,7 @@ public class MemberBillController {
 				b.setType(2);						//消费
 				b.setCardId(arr.getCardno());		//卡号
 				b.setMoney(arr.getPaid());			//消费金额
-				b.setTime(arr.getIntime().substring(0, 10));		//消费时间  yyyy-MM-dd
+				b.setTime(arr.getBilltime().substring(0, 10));		//消费时间  yyyy-MM-dd
 				b.setConsumetype(arr.getConsumetype());				//账单类型
 				list.add(b);
 			}else {
@@ -71,6 +79,7 @@ public class MemberBillController {
 		}
 		
 		List<ConsumeBill> allOther = memberBillservice.findAllOtherBill(map);
+
 		for(ConsumeBill arr:allOther) {
 			if(arr.getCardno().equals(cardno)) {					//判断卡号是否相等
 				Bill b = new Bill();
@@ -97,9 +106,13 @@ public class MemberBillController {
 	 */
 	@RequestMapping("/findbill")
 	public List<Bill> findBill(BillRequest bill) {		//需要传递参数卡号 手机号  起始时间和结束时间
+		List<String> type = reserveTimeService.findAllType();		//查询全部的场馆名称
+		List<String> Other = reserveTimeService.findAllOtherType();	//查询其他消费类型名称
 		Map<String,Object> map = new HashMap<String, Object>();		
 		map.put("cardno",bill.getCardId());
 		map.put("mobile",bill.getMobile());
+		map.put("list", type);
+		map.put("other", Other);
 		String start  =bill.getStarttime()+" 00:00:01";		//在yyyy-MM-dd  后面加入时分秒  
 		map.put("starttime",start);
 		String end  =bill.getEndtime()+" 23:59:59";
@@ -126,7 +139,7 @@ public class MemberBillController {
 				b.setType(2);						//消费
 				b.setCardId(arr.getCardno());		//卡号
 				b.setMoney(arr.getPaid());			//消费金额
-				b.setTime(arr.getIntime().substring(0, 10));		//消费时间  yyyy-MM-dd
+				b.setTime(arr.getBilltime().substring(0, 10));		//消费时间  yyyy-MM-dd
 				b.setConsumetype(arr.getConsumetype());				//消费类型
 				list.add(b);
 			}else {
